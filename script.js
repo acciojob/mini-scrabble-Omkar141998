f1641332281bed636b71bf4648de846a90e4cb36
Omkar141998 <120208397+Omkar141998@users.noreply.github.com>
2023-01-13 20:45:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+let count;
+document.getElementById("evaluatedText").value = ""; 
+
+document.addEventListener("keyup", function () {
+  count = document.getElementById("evaluatedText").value; 
+  document.querySelector("#letterCount").textContent = count.length;
+  //   console.log(count);
+});
